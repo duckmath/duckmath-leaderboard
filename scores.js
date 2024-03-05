8d6178b9dc74271e98889cfacba62d41eb3fdced
Maddox Schmidlkofer <maddoxschmidlkofer@gmail.com>
2024-03-04 21:44:40 -0500

    Update scores.js

--- scores.js
+++ scores.js
@@ -1,1 +1,1 @@
-Score("Slope" , "test" , 68, "assets/img/icon/slope.webp" , "g4m3s/slope.hmtl").add_new_score();
+new Score("Slope" , "test" , 68, "assets/img/icon/slope.webp" , "g4m3s/slope.hmtl").add_new_score();
