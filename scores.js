83b06edc038e05763958b04cf7365a0ecadfe595
Maddox Schmidlkofer <maddoxschmidlkofer@gmail.com>
2024-04-01 21:13:07 -0400

    Update scores.js

--- scores.js
+++ scores.js
@@ -1,8 +1,8 @@
 new Score("Slope" , "Maddox" , 68, "assets/img/icons/slope.webp" , "g4m3s/slope.html").add_new_score();
 
-new Score("Binding Of Isaac" , "RainbowPC" , 156, "assets/img/icons/isaac.webp" , "g4m3s/flash-g4m3s/thebindingofisaac.html").add_new_score();
+new Score("Binding Of Isaac" , "RainbowPC" , 164, "assets/img/icons/isaac.webp" , "g4m3s/flash-g4m3s/thebindingofisaac.html").add_new_score();
 
-new Score("Subway Surfers" , "Ethan" , 1508, "assets/img/icons/subway.webp" , "g4m3s/subway_surfers.html").add_new_score();
+new Score("Subway Surfers" , "Devin" , 3260, "assets/img/icons/subway.webp" , "g4m3s/subway_surfers.html").add_new_score();
 
 new Score("Drift Boss" , "Devin" , 3365, "assets/img/icons/driftboss.webp" , "g4m3s/driftboss.html").add_new_score();
 
