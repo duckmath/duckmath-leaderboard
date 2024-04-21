f96475dd2be30b8ec97121be2c9b37dea6ebfe12
Maddox Schmidlkofer <maddoxschmidlkofer@gmail.com>
2024-04-21 17:34:04 -0400

    Update scores.js

--- scores.js
+++ scores.js
@@ -1,4 +1,4 @@
-new Score("Slope" , "Maddox" , 68, "assets/img/icons/slope.webp" , "g4m3s/slope.html").add_new_score();
+new Score("Slope" , "Matt<3" , 113, "assets/img/icons/slope.webp" , "g4m3s/slope.html").add_new_score();
 
 new Score("Binding Of Isaac" , "RainbowPC" , 164, "assets/img/icons/isaac.webp" , "g4m3s/flash-g4m3s/thebindingofisaac.html").add_new_score();
 
